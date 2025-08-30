e71c4314eb884293688be2a3450858a0d12fd484
Alessandro Veloso <alessandroveloso5050@gmail.com>
2025-08-30 10:08:34 -0300

    ajustagem de padrão convencional e adição da funcionalidade startProgram

--- directory_management.py
+++ directory_management.py
@@ -2,16 +2,13 @@ from pathlib import Path
 class Directory:
     def __init__(self, abs_dir: Path):
         self._abs_dir = abs_dir
-
+    @property
+    def get(self):
+        return self._abs_dir
+    
     def back(self):
         self._abs_dir = self._abs_dir.parent
         return self._abs_dir
     def add(self, direc: str):
         self._abs_dir = self._abs_dir / direc
-
-    def __repr__(self):
-        return str(self._abs_dir)
-# def change(*p):
-#     print(p)
-# change(*[1,2,3,4])
-# print(*("aaaa")[1:])
+    
--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 from pathlib import Path
 import os
 from directory_management import Directory
-# O PROGRAMA ESTÁ ENVIANDO DADOS BRUTOS DE PARÂMENTROS, O QUE É NÃO CONVENCIONAL
 def changeDir(arg: tuple[str] = None):
     if arg[0] == None:
         print(PATHDIR)
     elif arg[0] == "..":
         PATHDIR.back()
     else:
-        __dir = str(PATHDIR) + "/" + arg[0]
+        __dir = PATHDIR.get / arg[0]
         if os.path.isdir(__dir):
             PATHDIR.add(arg[0])
         else:
             print(f"{__dir} é um diretório inválido.\n")
 def showSubDirs():
     print()
-    for subd in os.listdir(str(PATHDIR)):
+    for subd in os.listdir(PATHDIR.get):
         print("|", subd)
     print()
-def revert(index: tuple[int] = None):
+def revert(index: tuple[str] = None):
     def exec_history(index: int):
         if len(cmd_history[index]) > 1:
             print("->", *cmd_history[index])
@@ -42,13 +41,17 @@ def showHistory():
             print(f"'{cmd_history[i][j]}'", end=", ")
         print(")")
     print("-"*30)
+def startProgram(program_name):
+    file_path = PATHDIR.get / program_name[0]
+    if os.system(f"start {file_path} 2>nul") != 0:
+        print(f"O sistema não pôde encontrar o arquivo {file_path}\n")
 def execute(*functools: str):
     if catch_typing_errors(functools[0], len(functools) - 1):
         if len(functools) == 1:
             paramters[functools[0]][0]()
         else:
             paramters[functools[0]][0](functools[1:])
-def catch_typing_errors(command: str, qtparams: int):
+def catch_typing_errors(command: str, qtparams: int) -> bool:
     if command not in paramters.keys():
         print(f"'{command}' não é reconhecido como comando interno ou externo,\nprograma operável ou arquivo batch.\n")
         return False
@@ -58,17 +61,18 @@ def catch_typing_errors(command: str, qtparams: int):
         return False
     else:
         return True
-paramters = {"dir": (showSubDirs, 0),
+paramters: dict[str, tuple] = {"dir": (showSubDirs, 0),
              "cd": (changeDir, 1),
              "!!": (revert, 0),
              "!": (revert, 1),
              "exit": (exit, 0),
              "showh": (showHistory, 0),
+             "start": (startProgram, 1),
              }
-PATHDIR = Directory(Path(__file__).parent)
-cmd_history = []
+PATHDIR: Directory = Directory(Path(__file__).parent)
+cmd_history: list = []
 while True:
-    entrada = input(f"{PATHDIR}>").strip().split(" ")
+    entrada: str = input(f"{PATHDIR.get}>").strip().split(" ")
     cmd_history.append(tuple(entrada))
     if catch_typing_errors(entrada[0], len(entrada) - 1):
         execute(*entrada)
