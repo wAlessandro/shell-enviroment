05cfcb1805aaed658511216ba1f6ac030d1c7493
Alessandro Veloso <alessandroveloso5050@gmail.com>
2025-09-01 15:03:21 -0300

    adição das funcionalidades cls,color,help

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
 from pathlib import Path
 import os
 from directory_management import Directory
+#HELP
+def _help():
+    print("""
+CD          Altera o caminho atual de gerenciamento.
+!!          Executa o último comando digitado.
+!           Executa o comando indexado, a partir do histórico.
+EXIT        Fecha o terminal. 
+SHOWH       Imprime os comandos do histórico. 
+START       Executa um programa do diretório atual.
+CLS         Limpa o terminal.
+COLOR       Altera a cor do terminal.
+""")
+def changeColor(color: tuple):
+    colors = {"black": 0,
+              "blue": 1,
+              "green": 2,
+              "aqua": 3,
+              "red": 4,
+              "purple": 5,
+              "yellow": 6,
+              "white": 7}
+    os.system(f"COLOR {colors[color[0]]}")
+def clearTerminal():
+    os.system("cls")
+def showSubDirs():
+    print()
+    for subd in os.listdir(PATHDIR.get):
+        print("|", subd)
+    print()
+def showHistory():
+    print("_"*30)
+    for i in range(len(cmd_history)):
+        print(i,"->", cmd_history[i][0], "(", end="")
+        for j in range(1,len(cmd_history[i])):
+            print(f"'{cmd_history[i][j]}'", end=", ")
+        print(")")
+    print("-"*30)    
 def changeDir(arg: tuple[str] = None):
-    if arg[0] == None:
-        print(PATHDIR)
-    elif arg[0] == "..":
+    if arg[0] == "..":
         PATHDIR.back()
     elif arg[0].count(".") > 2:
         return
@@ -14,11 +49,6 @@ def changeDir(arg: tuple[str] = None):
             PATHDIR.add(arg[0])
         else:
             print(f"{__dir} é um diretório inválido.\n")
-def showSubDirs():
-    print()
-    for subd in os.listdir(PATHDIR.get):
-        print("|", subd)
-    print()
 def revert(index: tuple[str] = None):
     def exec_history(index: int):
         if len(cmd_history[index]) > 1:
@@ -35,24 +65,11 @@ def revert(index: tuple[str] = None):
             exec_history(int(index[0]))
     except IndexError:
         print("o histórico solicitado não está armazenado.\n")
-def showHistory():
-    print("_"*30)
-    for i in range(len(cmd_history)):
-        print(i,"->", cmd_history[i][0], "(", end="")
-        for j in range(1,len(cmd_history[i])):
-            print(f"'{cmd_history[i][j]}'", end=", ")
-        print(")")
-    print("-"*30)
-def startProgram(program_name):
+def startProgram(program_name: tuple):
     file_path = PATHDIR.get / program_name[0]
     if os.system(f"start {file_path} 2>nul") != 0:
         print(f"O sistema não pôde encontrar o arquivo {file_path}\n")
-def execute(*functools: str):
-    if catchTypingErrors(functools[0], len(functools) - 1):
-        if len(functools) == 1:
-            paramters[functools[0]][0]()
-        else:
-            paramters[functools[0]][0](functools[1:])
+
 def catchTypingErrors(command: str, qtparams: int) -> bool:
     if command not in paramters.keys():
         print(f"'{command}' não é reconhecido como comando interno ou externo,\nprograma operável ou arquivo batch.\n")
@@ -63,17 +80,27 @@ def catchTypingErrors(command: str, qtparams: int) -> bool:
         return False
     else:
         return True
-paramters: dict[str, tuple] = {"dir": (showSubDirs, 0),
+def execute(*functools: str):
+    if catchTypingErrors(functools[0], len(functools) - 1):
+        if len(functools) == 1:
+            paramters[functools[0]][0]()
+        else:
+            paramters[functools[0]][0](functools[1:])
+paramters: dict[str, tuple] = {
+            "dir": (showSubDirs, 0),
              "cd": (changeDir, 1),
              "!!": (revert, 0),
              "!": (revert, 1),
              "exit": (exit, 0),
              "showh": (showHistory, 0),
              "start": (startProgram, 1),
+             "cls": (clearTerminal, 0),
+             "color": (changeColor, 1),
+             "help": (_help, 0),
              }
 PATHDIR: Directory = Directory(Path(__file__).parent)
 cmd_history: list = []
 while True:
-    entrada: str = input(f"{PATHDIR.get}>").strip().split(" ")
+    entrada: str = input(f"{PATHDIR.get}>").lower().strip().split(" ")
     cmd_history.append(tuple(entrada))
     execute(*entrada)
